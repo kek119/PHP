dd68d7253c09465ccf0483c6956bb980460dcaa9
kek119 <dozywer@gmail.com>
2024-12-19 16:48:56 +0200

    Update lab8.php

--- lab8.php
+++ lab8.php
@@ -1,27 +1,26 @@
 <?php
-// URL із GET-параметрами
+
 $url = "http://lab.vntu.org/api-server/lab8.php?user=student&pass=p@ssw0rd";
 
-// Метод 1: Використання file_get_contents()
+
 $response = file_get_contents($url);
 
 if ($response === FALSE) {
     die("Помилка отримання даних за допомогою file_get_contents.");
 }
 
-// Розшифровуємо JSON у масив PHP
 $data = json_decode($response, true);
 
 if (json_last_error() !== JSON_ERROR_NONE) {
     die("Помилка розшифровки JSON: " . json_last_error_msg());
 }
 
-// Перевіримо формат отриманих даних
+
 if (!is_array($data)) {
     die("Дані, що повертаються з API, мають невірний формат.");
 }
 
-// Збираємо всі записи про людей
+
 $people = [];
 foreach ($data as $record) {
     if (is_array($record)) {
@@ -29,17 +28,16 @@ foreach ($data as $record) {
     }
 }
 
-// Виводимо дані в HTML-таблицю
 echo "<table border='1' cellpadding='5' cellspacing='0'>";
 echo "<tr>";
-// Динамічно створимо заголовки таблиці на основі першого запису
+
 if (!empty($people)) {
     foreach (array_keys($people[0]) as $header) {
         echo "<th>" . htmlspecialchars($header) . "</th>";
     }
     echo "</tr>";
 
-    // Виведемо всі рядки таблиці
+
     foreach ($people as $person) {
         echo "<tr>";
         foreach ($person as $value) {
